82bf0d4c80585fd349c341ce9f311d482b09d1ed
FitzHawke <will@fitzhawk.com>
2023-06-07 01:43:07 -0400

    fix: issues arising with dep updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,7 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  // Important!
-  // Only official '@astrojs/*' integrations are currently supported by Astro.
-  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
-  // with 'astro build' command.
-  experimental: {
-    integrations: true,
-  },
+  site: 'https://fitzhawke.com', // Use to generate your sitemap and canonical URLs in your final build.
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
--- src/components/react/GradientText.tsx
+++ src/components/react/GradientText.tsx
@@ -1,12 +1,12 @@
-import type { ReactNode } from 'react';
+// import type { ReactNode } from 'react';
 
 type IGradientTextProps = {
-  children: ReactNode;
+  text: string;
 };
 
 const GradientText = (props: IGradientTextProps) => (
   <span className="bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent">
-    {props.children}
+    {props.text}
   </span>
 );
 
