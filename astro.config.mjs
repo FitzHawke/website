ab71673ce6d22a27dc4d98ac74052f61c9cff79f
FitzHawke <will@fitzhawk.com>
2023-07-19 20:51:48 -0400

    chore: redo configs for eslint and typescript

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import robotsTxt from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://fitzhawke.com', // Use to generate your sitemap and canonical URLs in your final build.
+  site: "https://fitzhawke.com", // Use to generate your sitemap and canonical URLs in your final build.
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'monokai',
+      theme: "monokai",
     },
   },
   integrations: [react(), tailwind({}), sitemap(), robotsTxt()],
